02540e57c7e673a2474cb3f0801a4c676e2cc9df
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-10-07 20:25:55 -0300

    refactor(Refresh): change pattern for url validation

--- src/utils/isValidArticle.ts
+++ src/utils/isValidArticle.ts
@@ -11,15 +11,7 @@ const isValidArticle = ({ link, title, thumb }: ValidateParams) => {
   }
 
   function validURL(str: string) {
-    const pattern = new RegExp(
-      '^((https?:)?\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-      'i'
-    ); // fragment locator
+    const pattern = new RegExp('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?');
     return !!pattern.test(str);
   }
   if (!validURL(link) || !validURL(thumb)) {
